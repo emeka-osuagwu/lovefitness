959b9808099dff2be76d038d67f2a7decd367c01
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-20 18:50:19 +0100

    update 404 page

--- resources/views/pages/includes/sections/link.blade.php
+++ resources/views/pages/includes/sections/link.blade.php
@@ -4,3 +4,4 @@
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,300' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="{{  URL::asset('stylesheets/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('stylesheets/app.css') }}">
