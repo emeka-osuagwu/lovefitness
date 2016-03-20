d99a98d58f522df1649e830826c7cefa38bb4165
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-20 19:42:08 +0100

    add class page

--- resources/views/pages/class_list.blade.php
+++ resources/views/pages/class_list.blade.php
@@ -11,49 +11,57 @@
 				<h1>Book</h1>
 			</div>
 		</div>
-	</div
+	</div>
 
+	<div class="page-content">
 	
-<div class="page-content">
-
-	<div class="row filter-toolbar">
-		<div class="medium-3 columns filter-by">
-			<label>Filter by: </label>
-			<div class="filter-button">
-				<span>Popular <i class="fa fa-caret-down"></i></span>
-				<ul>
-					<li><a href="#">Popular</a></li>
-					<li><a href="#">Latest</a></li>
-				</ul>
+		<div class="row filter-toolbar">
+			<div class="medium-3 columns location">
+				<label>Location: </label>
+				<div class="filter-button">
+					<span>Lagos Fitness Gym <i class="fa fa-caret-down"></i></span>
+					<ul>
+						<li><a href="#">Gym 1</a></li>
+						<li><a href="#">Gym 2</a></li>
+						<li><a href="#">Gym 3</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
-		<div class="medium-2 columns checkbox-column">
-			<span class="checkbox-wrapper">
-			  <input type="checkbox">
-			</span>
-			<label>Checkbox</label>
-		</div>
-		<div class="medium-7 columns">
-			<a href="#" class="button orange">Martial Arts</a>
-			<a href="#" class="button blue">Cardio</a>
+
+		<div class="row">
+			<div class="medium-8 columns">
+				<main class="main-gym-list">
+					@include('pages.includes.contents.list_two')
+				</main>
+			</div>
+
+			<div class="medium-4 columns">
+				<aside class="sidebar">
+					
+					<div class="widget">
+						<h4 class="widget-title orange">Featured<i></i></h4>
+						<a href="#"><img src="images/featured.jpg" alt=""></a>
+					</div>
+
+					<div class="widget latest-posts">
+						<h4 class="widget-title orange">Latest Post<i></i></h4>
+						<ul>
+							<li>
+								<img src="images/image.jpg" alt="image" />
+								<div class="widget-content">
+									<h5><a href="http://localhost/lovefitness/post-2/">Post 2</a></h5>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus vel nibh in varius. Pellentesque eu elit quis mi tristique tempus in faucibus metus. Etiam lobortis placerat massa vestibulum scelerisque.</p>
+								</div>
+								<div class="meta clearfix">
+								 	<p>31 March 2015 </p> <a href="#"><i class="fa fa-chevron-circle-right"></i></a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</aside>  
+			</div>
 		</div>
 	</div>
 
-	<div class="row">
-		<div class="columns">
-			<main>
-				<ul class="medium-block-grid-3 class-categories">
-					<li>
-
-						<a href="#"><img src="images/swimming.jpg" alt=""></a>
-
-						<h3 class="red"><a href="#">Swimming</a></h3>
-
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod deleniti optio autem ducimus, minima recusandae corporis sunt, natus quos, consectetur sint voluptates! Hic voluptate quas, tempore cupiditate ipsum, illo explicabo non odio excepturi ducimus beatae sed ab temporibus minus. Laborum!</p>
-					</li>
-				</ul>
-			</main>
-		</div>
-	</div>
-</div>
 @endsection
---
+++ resources/views/pages/includes/contents/list_two.blade.php
@@ -0,0 +1,12 @@
+
+<div class="gym-item">
+	<img class="gym-image" src="images/gym.jpg" alt="">
+	<img class="gym-logo" src="images/gym-logo.png" alt="">
+	<div class="meta red clearfix">
+		<a href="#" class="arrow"></a>
+		<a href="#">CLASS: YOGA FOR BEGINNERS</a>
+		<a href="#">DURATION: 1H 30MIN</a>
+		<a href="#">GYM: ABC GYM</a>
+		<a href="#">LOCATION: IKOYI</a>
+	</div>
+</div>
