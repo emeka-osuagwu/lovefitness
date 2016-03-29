276783168b5b66f99c8ba137e9c8ad944a593689
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-29 22:54:19 +0100

    add helper function

--- resources/views/dashboard/pages/includes/sections/link.blade.php
+++ resources/views/dashboard/pages/includes/sections/link.blade.php
@@ -6,25 +6,25 @@
         <meta content="" name="author" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/simple-line-icons/simple-line-icons.min.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/uniform/css/uniform.default.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/simple-line-icons/simple-line-icons.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/uniform/css/uniform.default.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') !!}" rel="stylesheet" type="text/css" />
         <!-- END GLOBAL MANDATORY STYLES -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <link href="{!! asset('../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/morris/morris.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/fullcalendar/fullcalendar.min.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/global/plugins/jqvmap/jqvmap/jqvmap.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/morris/morris.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/fullcalendar/fullcalendar.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/plugins/jqvmap/jqvmap/jqvmap.css') !!}" rel="stylesheet" type="text/css" />
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="{!! asset('../assets/global/css/components.min.css') !!}" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="{!! asset('../assets/global/css/plugins.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/global/css/components.min.css') !!}" rel="stylesheet" id="style_components" type="text/css" />
+        <link href="{!! load_asset('../assets/global/css/plugins.min.css') !!}" rel="stylesheet" type="text/css" />
         <!-- END THEME GLOBAL STYLES -->
         <!-- BEGIN THEME LAYOUT STYLES -->
-        <link href="{!! asset('../assets/layouts/layout4/css/layout.min.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('../assets/layouts/layout4/css/themes/light.min.css') !!}" rel="stylesheet" type="text/css" id="style_color" />
-        <link href="{!! asset('../assets/layouts/layout4/css/custom.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/layouts/layout4/css/layout.min.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! load_asset('../assets/layouts/layout4/css/themes/light.min.css') !!}" rel="stylesheet" type="text/css" id="style_color" />
+        <link href="{!! load_asset('../assets/layouts/layout4/css/custom.min.css') !!}" rel="stylesheet" type="text/css" />
         <!-- END THEME LAYOUT STYLES -->
         <link rel="shortcut icon" href="favicon.ico" />
