21c65da7a7fae88e6f4218c4d73468ef2716796b
Emeka Osuagwu <you@example.com>
2016-04-09 16:22:10 +0100

    fix issues

--- resources/views/dashboard/pages/includes/sections/left_nav.blade.php
+++ resources/views/dashboard/pages/includes/sections/left_nav.blade.php
@@ -22,7 +22,6 @@
 					</li>
 					<li>
 						<a href="/gyms/">
-						<i class="icon-list"></i><span class="badge badge-info">3</span>
 						List all</a>
 					</li>
 				</ul>
@@ -53,17 +52,14 @@
 					</li>
 					<li>
 						<a href="/dashboard/class/">
-						<i class="icon-list"></i><span class="badge badge-info">3</span>
 						List all Classes</a>
 					</li>
 					<li>
 						<a href="{{ Url('dashboard/class/groups') }}">
-						<i class="icon-list"></i><span class="badge badge-info">3</span>
 						List all Group</a>
 					</li>
 					<li>
 						<a href="{{ Url('dashboard/class/categorys') }}">
-						<i class="icon-list"></i><span class="badge badge-info">3</span>
 						List all Category</a>
 					</li>
 				</ul>
