5c3078c103784bd10114b883e0c5127f4b9e8881
Emeka Osuagwu <you@example.com>
2016-04-10 14:26:48 +0100

    add venue migration

---
+++ database/migrations/2016_04_10_132631_create_venue_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVenueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('venues');
+    }
+}
