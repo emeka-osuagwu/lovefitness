a0611004a7c6f2f1a48e6c560ff386cea9565b5e
Emeka Osuagwu <you@example.com>
2016-04-18 14:00:33 +0100

    add cancel to page category

--- resources/views/dashboard/pages/includes/contents/edit_category.blade.php
+++ resources/views/dashboard/pages/includes/contents/edit_category.blade.php
@@ -61,6 +61,7 @@
 									<div class="row">
 										<div class="col-md-offset-3 col-md-9">
 											<button type="submit" class="btn green">Submit</button>
+											<a href="http://localhost:8000/dashboard/class/categorys" class="btn default">Cancel</a>
 										</div>
 									</div>
 								</div>
