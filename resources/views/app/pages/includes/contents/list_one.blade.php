2872d7fc7220c2ddb57076f4d9e27cc571b9461f
Emeka Osuagwu <you@example.com>
2016-06-07 15:07:13 +0100

    update readme

--- resources/views/app/pages/includes/contents/list_one.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="home-boxes">
-	<div class="row">
-		<div class="medium-4 columns">
-			<a href="#"><img src="images/review-gym.jpg" alt=""></a>
-			<h3 class="red"><a href="#">Find/review a gym</a></h3>
-		</div>
-	</div>
-</section>
--- resources/views/app/pages/includes/sections/script.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="{!! load_asset('bower_components/jquery/dist/jquery.min.js') !!}"></script>
-<script src="{!! load_asset('bower_components/foundation/js/foundation.min.js') !!}"></script>
-<script src="{!! load_asset('bower_components/PACE/pace.min.js') !!}"></script>
-<script src="{!! load_asset('js/app.js') !!}"></script>
-
